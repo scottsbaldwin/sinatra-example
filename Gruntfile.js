35582280e0b2cb5f6cf99171baa69b70beb498de
Jeff Gonzalez <likwid@gmail.com>
2014-07-15 14:03:23 -0500

    Added gitSha to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,10 @@
 /*jslint unparam:true */
 module.exports = function (grunt) {
     "use strict";
-    grunt.registerTask('heroku:development', 'clean less mincss');
+    grunt.registerTask(
+        "gitSha",
+        "register the current git information",
+        require("./gruntlib/tasks/gitSha.js")(grunt)
+    );
+    grunt.registerTask('heroku:development', 'gitSha');
 };
